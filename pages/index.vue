930905e0529657a4a001dda7c4d986af89d65e74
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2018-05-07 15:22:53 -0700

    Add a css transition al naveg. entre pages.

--- pages/index.vue
+++ pages/index.vue
@@ -10,4 +10,12 @@
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   padding: 60px;
 }
+
+.page-enter-active, .page-leave-active {
+  transition: all .25s ease-out;
+}
+.page-enter, .page-leave-active {
+  opacity: 0;
+  transform-origin: 50% 50%;
+}
 </style>
