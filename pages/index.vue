594a06ef1d1d639d8a14eb22a90d497c2ab4b9f3
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2018-05-07 15:47:52 -0700

    Add a js transition with gsap.

--- pages/index.vue
+++ pages/index.vue
@@ -66,3 +66,37 @@ a, a:visited {
   }
 }
 </style>
+
+<script type="text/javascript">
+  import { TweenMax, Back } from 'gsap'
+
+  export default {
+    transition: {
+      mode: 'out-in',
+      css: false,
+      beforeEnter (el) {
+        TweenMax.set(el, {
+          transformPerspective: 600,
+          perspective: 300,
+          transformStyle: 'preserve-3d'
+        })
+      },
+      enter (el, done) {
+        TweenMax.to(el, 1, {
+          rotationY: 360,
+          transformOrigin: '50% 50%',
+          ease: Back.easeOut
+        })
+        done()
+      },
+      leave (el, done) {
+        TweenMax.to(el, 1, {
+          rotationY: 0,
+          transformOrigin: '50% 50%',
+          ease: Back.easeIn
+        })
+        done()
+      }
+    }
+  }
+</script>
