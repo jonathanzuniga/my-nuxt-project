c8c9d59463765148babc2e13484ce021ef0f8b69
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2018-05-07 16:24:00 -0700

    Add a common nav. across all of the pages.

---
+++ components/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <div class="title">
+      <nuxt-link exact to="/">Home page</nuxt-link>
+      <nuxt-link to="/product">Product page</nuxt-link>
+      <nuxt-link to="/contact">Contact page</nuxt-link>
+    </div>
+  </nav>
+</template>
