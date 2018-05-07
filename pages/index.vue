aa4c4cb5275e3563906fdfd862e30af4bf1a8b46
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2018-05-07 15:28:52 -0700

    Add a new page (contact) and apply a diff. transition: Fade.

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <h1>Welcome!</h1>
     <p><nuxt-link to="/product">Product page</nuxt-link></p>
+    <p><nuxt-link to="/contact">Contact page</nuxt-link></p>
   </div>
 </template>
 
